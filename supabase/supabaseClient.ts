cb1ca782d7b65cdc0918eca3e47d272dcee7a0e6
inhwa <jeoninhwa3@naver.com>
2024-07-22 11:58:18 +0900

    feat: supabase 타입 추가

--- supabase/supabaseClient.ts
+++ supabase/supabaseClient.ts
@@ -1,6 +1,7 @@
+import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
