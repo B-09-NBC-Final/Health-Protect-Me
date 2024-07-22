9eefd582f907bd3632789e961cb237d5598e69a5
Joohwan Seo <songqkaoswe@naver.com>
2024-07-22 09:13:18 +0900

    initial settting

--- src/app/(main)/(contents)/posting-detail/[id]/page.tsx
+++ src/app/(main)/(contents)/posting-detail/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const PostingDetailPage = () => {
+const PostingDetailPage = ({ params }: { params: { id: string } }) => {
+  console.log(params.id);
   return <div>PostingDetailPage</div>;
 };
 
