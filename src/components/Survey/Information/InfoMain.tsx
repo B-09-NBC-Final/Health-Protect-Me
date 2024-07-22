448ea79d41488a27777109c2c8aa280b1995975a
Joohwan Seo <songqkaoswe@naver.com>
2024-07-22 22:34:05 +0900

    style: info-main 컴포넌트 css 추가

--- src/components/Survey/Information/InfoMain.tsx
+++ src/components/Survey/Information/InfoMain.tsx
@@ -3,12 +3,16 @@ import Link from 'next/link';
 const InfoMain = () => {
   return (
     <div>
-      <div>
-        <h2> 더이상 고민 없는, 나만의 영양사가 맞춰준 나를 위한 식단!</h2>
-        <p>나의 정보를 입력하고 나를 위한 맞춤 식단을 받아 보세요</p>
+      <div className="wx-3 justify-center content-center col-start-2 col-span-4 ">
+        <h2 className="m-1 h-10 border-solid border-y-2 ">더이상 고민 없는, 나만의 영양사가 맞춰준 나를 위한 식단!</h2>
+        <p className="m-1 h-10 items-center border-solid border-y-2 ">
+          나의 정보를 입력하고 나를 위한 맞춤 식단을 받아 보세요
+        </p>
       </div>
       <div>
-        <Link href="/info-reaserch">진단하러 가기</Link>
+        <Link href="/info-reaserch" className="col-start-1 col-end-7 border-dashed border-2 rounded">
+          진단하러 가기
+        </Link>
       </div>
     </div>
   );
