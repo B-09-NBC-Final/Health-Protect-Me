e0939d852fb22eb253150208293b8baa7909effc
Jony <104831702+JoohwanSeo@users.noreply.github.com>
2024-07-22 21:01:38 +0900

    Update Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const Header: React.FC = () => {
+const Header = () => {
   return (
-    <header className="bg-black text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <img src="/logo.png" alt="logo" className="w-12 h-12" />
-        <button className="px-4 py-2 bg-green-600 rounded">로그인</button>
-      </div>
-    </header>
+    <>
+      Header 
+    </>
   );
 };
 
