fbf9ea3ca77f21a2e9e8443d076e3af3bb007950
tdixxgidt@gmail.com <tdixxgidt@gmail.com>
2024-07-23 14:42:10 +0900

    feat:게시물 섹션과 프로필 섹션 스타일 수정

--- src/app/(main)/my-page/page.tsx
+++ src/app/(main)/my-page/page.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import MyPage from '@/components/MyPage';
-import MainLayout from '@/app/(main)/layout';
 
-const MyPageComponent: React.FC = () => {
-  return (
-    <MainLayout>
-      <MyPage />
-    </MainLayout>
-  );
-};
+const MyPageComponent: React.FC = () => <MyPage />;
 
 export default MyPageComponent;
