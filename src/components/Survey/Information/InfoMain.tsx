7b9c2d001062a4b40eec734ee9031c1cd2387e96
Joohwan Seo <songqkaoswe@naver.com>
2024-07-23 11:43:30 +0900

    add: footer 컴포넌트 추가

--- src/components/Survey/Information/InfoMain.tsx
+++ src/components/Survey/Information/InfoMain.tsx
@@ -2,17 +2,24 @@ import Link from 'next/link';
 
 const InfoMain = () => {
   return (
-    <div>
-      <div className="wx-3 justify-center content-center col-start-2 col-span-4 ">
-        <h2 className="m-1 h-10 border-solid border-y-2 ">더이상 고민 없는, 나만의 영양사가 맞춰준 나를 위한 식단!</h2>
-        <p className="m-1 h-10 items-center border-solid border-y-2 ">
-          나의 정보를 입력하고 나를 위한 맞춤 식단을 받아 보세요
-        </p>
-      </div>
-      <div>
-        <Link href="/info-reaserch" className="col-start-1 col-end-7 border-dashed border-2 rounded">
-          진단하러 가기
-        </Link>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-2xl w-full mx-auto p-6 bg-white shadow-lg rounded-lg">
+        <div className="text-center mb-8">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 py-2 border-gray-200">
+            더이상 고민 없이, 나만의 영양사가 맞춰준 나를 위한 식단!
+          </h1>
+          <p className="text-lg text-gray-600 py-2  border-gray-200">
+            나의 정보를 입력하고 나만을 위한 맞춤 식단을 받아 보세요
+          </p>
+        </div>
+        <div className="text-center">
+          <Link
+            href="/info-reaserch"
+            className="inline-block px-6 py-3 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            진단하러 가기
+          </Link>
+        </div>
       </div>
     </div>
   );
