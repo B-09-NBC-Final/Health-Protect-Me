ac1b9c1a131b4aa8ed73c2b545ceecf11bca9cdc
tdixxgidt@gmail.com <tdixxgidt@gmail.com>
2024-07-23 17:52:28 +0900

    feat: 게시물 섹션과 프로필 섹션 스타일 수정

--- src/components/MyPage/PostsSection.tsx
+++ src/components/MyPage/PostsSection.tsx
@@ -13,7 +13,7 @@ const PostsSection = () => {
             </div>
             <h3 className="text-green-500 font-semibold mb-2 truncate">잡담</h3>
             <p className="text-gray-700 font-semibold mb-2 truncate">제목입니다. 최대 1줄까지 생각중이에요</p>
-            <p className="text-gray-700 mb-2 truncate-lines-2">
+            <p className="text-gray-700 mb-2 overflow-hidden text-ellipsis">
               내용이에요. 최대 2줄까지 생각하고 있습니다. 공간 여백..
             </p>
             <time className="text-gray-400">24. 7. 14.</time>
