753902d45d705c4f848126af962765dfe4ae9c27
Joohwan Seo <songqkaoswe@naver.com>
2024-07-24 10:56:25 +0900

    add: supabase/add to middleware component

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,5 +1,9 @@
+import { Database } from '@/types/supabase';
 import { createBrowserClient } from '@supabase/ssr';
 
 export function createClient() {
-  return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+  return createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 }
