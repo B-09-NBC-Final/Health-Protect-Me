a746259730e9db617e4020041309c4a8e8152d28
Joohwan Seo <songqkaoswe@naver.com>
2024-07-24 13:36:41 +0900

    style: css 일부 수정

--- src/components/Contents/Posting/Textarea/TextareatPage.tsx
+++ src/components/Contents/Posting/Textarea/TextareatPage.tsx
@@ -6,18 +6,20 @@ const TextareaPage = () => {
   return (
     <div className="max-w-2xl mx-auto p-6">
       <div className="mb-8">
-        <h1 className="text-3xl font-bold mb-4">커뮤니티 글 작성</h1>
+        <h1 className="text-4xl font-bold mb-4">커뮤니티 글 작성</h1>
+        <br />
+        <p className="mt-1 text-sm text-black">당신의 이야기를 들려주세요.</p>
         <Input
           type="text"
           placeholder="제목을 입력 해주세요."
-          className="w-full p-2 mb-4 border-b border-transparent focus:border-[#FF7A85] focus:outline-none transition-colors duration-300 placeholder-gray-400 text-black"
+          className="mt-10 w-full p-2 mb-4 border-b border-transparent focus:border-[#FF7A85] focus:outline-none transition-colors duration-300 placeholder-gray-400 text-black"
         />
+        <p className="border-solid border-2"></p>
         <Textarea
           placeholder="내용을 적어주세요."
           id="message-2"
-          className="w-full h-64 p-4 border-transparent rounded-lg focus:ring-2 focus:ring-[#FF7A85] focus:border-[#FF7A85] outline-none transition-all duration-300 resize-none placeholder-gray-400 text-black"
+          className="mt-5 w-full h-64 p-4 border-transparent rounded-lg focus:ring-2 focus:ring-[#FF7A85] focus:border-[#FF7A85] outline-none transition-all duration-300 resize-none placeholder-gray-400 text-black"
         />
-        <p className="mt-2 text-sm text-black">당신의 이야기를 들려주세요.</p>
       </div>
       <TextareatBtn />
     </div>
