d7c7826c8fe7397e6a70e0d8ee3b320bc8ebabf8
Joohwan Seo <songqkaoswe@naver.com>
2024-07-24 12:38:25 +0900

    update: 커뮤니티 게시글 작성 UI 구현 완료

--- src/components/Contents/Posting/Textarea/TextareatPage.tsx
+++ src/components/Contents/Posting/Textarea/TextareatPage.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
 import { Textarea } from '@/components/ui/textarea';
-import { Button } from '@/components/ui/button';
+import TextareatBtn from '../Btn/TextareatBtn';
+import { Input } from '@/components/ui/input';
 
 const TextareaPage = () => {
   return (
     <div className="max-w-2xl mx-auto p-6">
       <div className="mb-8">
         <h1 className="text-3xl font-bold mb-4">커뮤니티 글 작성</h1>
-        <Textarea
-          placeholder="당신의 이야기를 적어주세요."
-          id="message-2"
-          className="w-full h-64 p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none"
+        <Input
+          type="text"
+          placeholder="제목을 입력 해주세요."
+          className="w-full p-2 mb-4 border-b border-transparent focus:border-[#FF7A85] focus:outline-none transition-colors duration-300 placeholder-gray-400 text-black"
         />
-        <p className="mt-2 text-sm text-gray-500">많은 사람들이 당신의 글을 볼 수 있어요.</p>
+        <Textarea
+          placeholder="내용을 적어주세요."
+          id="message-2"
+          className="w-full h-64 p-4 border-transparent rounded-lg focus:ring-2 focus:ring-[#FF7A85] focus:border-[#FF7A85] outline-none transition-all duration-300 resize-none placeholder-gray-400 text-black"
+        />
+        <p className="mt-2 text-sm text-black">당신의 이야기를 들려주세요.</p>
       </div>
-      <div className="flex justify-end space-x-4">
-        <Button className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">
-          등록하기
-        </Button>
-        <Button className="px-6 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition duration-300">
-          취소하기
-        </Button>
-      </div>
+      <TextareatBtn />
     </div>
   );
 };
