049b36bfa18601607f37c2357dbaee6748525e90
inhwa <jeoninhwa3@naver.com>
2024-07-25 09:49:30 +0900

    refactor: 컴포넌트 정리

--- src/app/(main)/(contents)/posting-detail/[id]/page.tsx
+++ src/app/(main)/(contents)/posting-detail/[id]/page.tsx
@@ -1,21 +1,14 @@
+import { useEffect, useState } from 'react';
+import { Post, User } from '@/types';
 import PostingDetailPost from '@/components/Contents/PostingDetail/PostingDetailPost';
-import PostingDetailUser from '@/components/Contents/PostingDetail/PostingDetailUser';
+import PostingDetailBtn from '@/components/Contents/PostingDetail/PostingDetailBtn';
 
 const PostingDetailPage = ({ params }: { params: { id: string } }) => {
   return (
-    // 팀원들이랑 상의해서 main 공통으로 빼기
     <main className="min-h-screen bg-gray-100">
       <div className="mx-auto max-w-[1280px] p-[100px]">
         <PostingDetailPost params={params} />
-        <PostingDetailUser />
-        <div className="mt-5 text-right">
-          <button type="button" className="p-3 bg-gray-200">
-            수정하기
-          </button>
-          <button type="button" className="p-3 bg-gray-200 ml-5">
-            삭제하기
-          </button>
-        </div>
+        <PostingDetailBtn params={params} />
       </div>
     </main>
   );
--- src/components/Contents/PostingDetail/PostingDetailPost.tsx
+++ src/components/Contents/PostingDetail/PostingDetailPost.tsx
@@ -3,28 +3,67 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { createClient } from '@/supabase/client';
-import { Post } from '@/types';
+import { Post, User } from '@/types';
 
 const PostingDetailPost = ({ params }: { params: { id: string } }) => {
   const { id } = params;
   const supabase = createClient();
   const [post, setPost] = useState<Post | null>(null);
+  const [user, setUser] = useState<User | null>(null);
 
   const getPost = async () => {
-    const { data, error } = await supabase.from('posts').select().eq('id', id).single();
-    setPost(data);
-  };
+    const { data: getPost, error } = await supabase.from('posts').select('*').eq('id', id).single();
 
+    if (error) {
+      console.log('error', error);
+      return;
+    }
+    setPost(getPost);
+
+    const { data: getUser } = await supabase
+      .from('users')
+      .select('*')
+      .eq('user_id', post?.user_id as string)
+      .single();
+
+    if (error) {
+      console.log('error', error);
+    } else {
+      setUser(getUser);
+    }
+  };
+  console.log(post?.user_id);
+  // const getUser = async () => {
+  //   const session = await supabase.auth.getSession();
+  //   const isSignIn = !!session.data.session;
+  //   console.log(isSignIn);
+
+  //   console.log('aaaa', post);
+
+  // };
   useEffect(() => {
     getPost();
+    // getUser();
   }, []);
 
+  // console.log(user);
+
   return (
-    <div>
-      <p className="font-bold border-b-2 border-solid border-coral pb-1 mb-5">{post?.title}</p>
-      <Image src={post?.image_url as string} alt="" width={200} height={200} className="mb-3 rounded-xl" />
-      <p className="mb-5">{post?.content}</p>
-    </div>
+    <>
+      <div>
+        <p className="font-bold border-b-2 border-solid border-coral pb-1 mb-5">{post?.title}</p>
+        <Image src={post?.image_url as string} alt="" width={200} height={200} className="mb-3 rounded-xl" />
+        <p className="mb-5">{post?.content}</p>
+      </div>
+
+      <div className="flex border-y-2 border-solid border-coral py-5">
+        <Image src="" alt="" width={50} height={50} className="rounded-full" />
+        <div className="flex flex-col ml-5">
+          <strong>{user?.nickname}</strong>
+          <span>24.7.18</span>
+        </div>
+      </div>
+    </>
   );
 };
 
