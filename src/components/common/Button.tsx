65d4a792d10289db351aab8947c382e93e08c7e9
inhwa <jeoninhwa3@naver.com>
2024-07-31 02:50:51 +0900

    feat: 로그인 여부에 따른 라우팅 기능 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -16,9 +16,9 @@ const Button = ({ buttonName, bgColor, textColor, paddingY, marginY, buttonWidth
     <button
       className={`${bgColor ? bgColor : 'bg-[#FF7A85]'} rounded-lg ${
         textColor ? textColor : 'text-white'
-      } font-semibold ${marginY ? marginY : 'ps-3.5'} ${paddingY ? paddingY : 'py-4'} ${
+      } font-semibold ${marginY ? marginY : 'px-0'} ${paddingY ? paddingY : 'py-4'} ${
         hover ? 'hover:box-shadow: 0px 8px 16px 0px rgba(255, 122, 133, 0.40)' : ''
-      }  ${buttonWidth ? buttonWidth : null}`}
+      }  ${buttonWidth ? buttonWidth : 'w-[240px]'}`}
       onClick={onClick}
     >
       {buttonName}
