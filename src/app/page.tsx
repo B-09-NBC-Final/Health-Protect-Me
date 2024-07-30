34ba75fc33f32a5e4cb469af8c9a1ca512125793
inhwa <jeoninhwa3@naver.com>
2024-07-31 02:10:03 +0900

    feat: 버튼 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 import MainSwiper from '@/components/MainPage/MainSwiper';
+import Image from 'next/image';
+import Thumbnail from '@/assets/image/img_thumbnail.svg';
+import Button from '@/components/common/Button';
 
 const MainPage = () => {
+  const checkDiet = () => {};
   return (
     <main>
+      <div className="inner_wrap">
+        <div className="flex justify-between items-end py-40">
+          <div>
+            <h2 className="text-5xl font-medium leading-tight mb-10">
+              간편하게, 나만을 위한 <br />
+              맞춤형 건강 솔루션
+            </h2>
+            <Button buttonName="맞춤 식단 제공받기" onClick={checkDiet}></Button>
+          </div>
+          <Image src={Thumbnail} alt="간편하게, 나만을 위한 맞춤형 건강 솔루션" width={790} height={606} />
+        </div>
+      </div>
+
       <div className="swiper-area">
         <div className="flex justify-between items-center">
           <MainSwiper reverseDirection={true} />
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,29 @@
+import { MouseEvent } from 'react';
+
+interface ButtonProps {
+  buttonName: string;
+  bgColor?: string;
+  textColor?: string;
+  paddingY?: string;
+  marginY?: string;
+  buttonWidth?: string;
+  hover?: boolean;
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button = ({ buttonName, bgColor, textColor, paddingY, marginY, buttonWidth, hover, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`${bgColor ? bgColor : 'bg-[#FF7A85]'} rounded-lg ${
+        textColor ? textColor : 'text-white'
+      } font-semibold ${marginY ? marginY : 'ps-3.5'} ${paddingY ? paddingY : 'py-4'} ${
+        hover ? 'hover:box-shadow: 0px 8px 16px 0px rgba(255, 122, 133, 0.40)' : ''
+      }  ${buttonWidth ? buttonWidth : null}`}
+      onClick={onClick}
+    >
+      {buttonName}
+    </button>
+  );
+};
+
+export default Button;
