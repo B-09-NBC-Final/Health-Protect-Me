1d54ec0f960156cc01cc26eac76c2c01cdb497a2
Joohwan Seo <songqkaoswe@naver.com>
2024-07-31 17:09:12 +0900

    add: infomain 내 layout 컴포넌트 생성

--- src/app/(infoMain)/info-reaserch/page.tsx
+++ src/app/(infoMain)/info-reaserch/page.tsx
@@ -1,11 +1,9 @@
-import Header from '@/components/common/Header/Header';
 import InfoReaserch from '@/components/Survey/Information/Reaserch/InfoReaserch';
 import React from 'react';
 
 const InfoReaserchPage = () => {
   return (
     <div>
-      <Header />
       <InfoReaserch />
     </div>
   );
--- src/app/(infoMain)/info/page.tsx
+++ src/app/(infoMain)/info/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import InfoMain from '@/components/Survey/Information/InfoMain';
-import Header from '@/components/common/Header/Header';
 
 const infoPage = () => {
   return (
     <div>
-      <Header />
       <InfoMain />
     </div>
   );
