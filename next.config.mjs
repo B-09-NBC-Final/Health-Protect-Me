60d8334d3f23881875f92611be44b4c5beeb369f
Elephant24456 <rlaxortn0984@naver.com>
2024-08-02 04:21:17 +0900

    update: print gpt data

--- next.config.mjs
+++ next.config.mjs
@@ -13,15 +13,9 @@ const nextConfig = {
         hostname: 'qdtbndpvityeryaniyfj.supabase.co',
         pathname: '/**'
       },
-<<<<<<< HEAD
-      {
-        hostname: 'lh3.googleusercontent.com'
-      }
-=======
       // {
       //   hostname: 'lh3.googleusercontent.com'
       // }
->>>>>>> 444a35ab69c1db6e2ba2294509447d06cf41b3ea
     ]
   },
   env: {
