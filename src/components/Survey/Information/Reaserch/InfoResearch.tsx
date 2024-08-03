1123e534f52364101cef1569bdf281d95156e1b7
Joohwan Seo <songqkaoswe@naver.com>
2024-08-03 15:49:02 +0900

    update: 설문조사 페이지 - 체중 항목 오류 수정

--- src/components/Survey/Information/Reaserch/InfoResearch.tsx
+++ src/components/Survey/Information/Reaserch/InfoResearch.tsx
@@ -44,10 +44,19 @@ const InfoResearch = (): JSX.Element => {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setSurveyData((prevData) => ({
-      ...prevData,
-      [name]: ['height', 'weight', 'year_of_birth'].includes(name) ? (value === '' ? null : Number(value)) : value
-    }));
+    if (name === 'weight') {
+      // 숫자만 허용하고, 빈 문자열이면 null로 설정함.
+      const numericValue = value.replace(/[^0-9]/g, '');
+      setSurveyData((prevData) => ({
+        ...prevData,
+        [name]: numericValue === '' ? null : Number(numericValue)
+      }));
+    } else {
+      setSurveyData((prevData) => ({
+        ...prevData,
+        [name]: ['height', 'year_of_birth'].includes(name) ? (value === '' ? null : Number(value)) : value
+      }));
+    }
   };
 
   const handleGenderSelect = (gender: Gender): void => {
