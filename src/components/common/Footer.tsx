06014d795dc162d049ccb1e772dca97e81cb122d
heesu21 <gmltncpfl1@naver.com>
2024-08-04 19:05:02 +0900

    feat:footeruiux

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -6,13 +6,21 @@ import instagramIcon from '@/assets/icons/icon_instar.png';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-white z-10 absolute bottom-0 left-0">
-      <div className="inner_wrap flex justify-between items-center px-10 py-4">
-        <div className="flex items-center">
+    <footer className="w-full bg-white z-10 absolute bottom-0 left-0 p-6">
+      <div className="inner_wrap flex items-center justify-between px-10 py-4">
+        <div className="flex items-center p-6 gap">
           <Link href={'/'}>
             <Image src={logo} alt="logo" width={100} height={28} />
           </Link>
+          <div className="flex space-x-4 ml-4 p-4 ">
+            <Image src={githubIcon} alt="GitHub" width={24} height={24} />
+            <Image src={instagramIcon} alt="Instagram" width={24} height={24} />
+          </div>
         </div>
+      </div>
+      <div className="inner_wrap">
+        <p className="text-sm text-color-gray px-6">P리부는P리</p>
+        <p className="text-sm text-color-gray px-6">Copyright 2024. HELPME. All right reserved.</p>
       </div>
     </footer>
   );
