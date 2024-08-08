96d87fa725d7198e4ef7e13dbcfd4700c69a6c1b
heesu21 <gmltncpfl1@naver.com>
2024-08-08 23:02:03 +0900

    fix: 동기화진행중/타입에러수정

--- src/components/MyPage/EditProfile/Form/ProfileImg.tsx
+++ src/components/MyPage/EditProfile/Form/ProfileImg.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import defaultimg from '@/assets/image/defaultimg.png';
 
 type ProfileImageProps = {
-  profileImage: string
-  onImageUpload: (event: React.ChangeEvent<HTMLInputElement>) => void
-}
+  profileImage: string;
+  onImageUpload: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 const ProfileImage: React.FC<ProfileImageProps> = ({ profileImage, onImageUpload }) => {
   return (
@@ -12,26 +13,20 @@ const ProfileImage: React.FC<ProfileImageProps> = ({ profileImage, onImageUpload
       <div className="w-24 h-24 rounded-full bg-gray-300 flex items-center justify-center relative">
         <Image
           className="rounded-full cursor-pointer"
-          src={profileImage || '/path/to/default-profile-image.jpg'}
+          src={profileImage || defaultimg}
           alt="Profile"
           width={120}
           height={120}
           onClick={() => document.getElementById('fileInput')?.click()}
         />
-        <input
-          type="file"
-          accept="image/*"
-          id="fileInput"
-          className="hidden"
-          onChange={onImageUpload}
-        />
+        <input type="file" accept="image/*" id="fileInput" className="hidden" onChange={onImageUpload} />
       </div>
       <div className="flex flex-col text-left">
         <div className="w-[272px] mb-1">프로필 사진</div>
         <div className="w-[272px] text-sm text-[#76797F]">5MB 이하의 PNG, JPG 파일을 올려주세요.</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileImage
+export default ProfileImage;
