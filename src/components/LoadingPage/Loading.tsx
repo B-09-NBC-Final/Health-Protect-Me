a7d84791f98170a8eb41c91b0b67ac4fd2bd7800
Joohwan Seo <songqkaoswe@naver.com>
2024-08-08 09:10:10 +0900

    add: 로딩화면 구성 중

--- src/components/LoadingPage/Loading.tsx
+++ src/components/LoadingPage/Loading.tsx
@@ -1,8 +1,23 @@
-'use client'
-import React from "react";
+import styles from './Loading.module.css'
 
-const Loading = () => {
-    return <div>Loading..!</div>;
+const LoadingPage = () => {
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.loadingIcon}>
+        <div className={`${styles.petal} ${styles.petal1}`}></div>
+        <div className={`${styles.petal} ${styles.petal2}`}></div>
+        <div className={`${styles.petal} ${styles.petal3}`}></div>
+        <div className={`${styles.petal} ${styles.petal4}`}></div>
+      </div>
+      <div className={styles.loadingText}>
+        {'Loading...'.split('').map((char, index) => (
+          <span key={index} style={{ animationDelay: `${0.1 * (index + 1)}s` }}>
+            {char}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Loading;
+export default LoadingPage;
