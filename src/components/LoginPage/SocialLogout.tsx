0eabcbabadd00e674ba03b07967cf6bd75048f13
heesu21 <gmltncpfl1@naver.com>
2024-08-13 00:43:57 +0900

    feat: 로그아웃알럿창

--- src/components/LoginPage/SocialLogout.tsx
+++ src/components/LoginPage/SocialLogout.tsx
@@ -3,6 +3,17 @@
 import { useUserStore } from '@/store/userStore';
 import { createClient } from '@/supabase/client';
 import { useRouter } from 'next/navigation';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog';
 
 const SignOutButton = () => {
   const { clearUser } = useUserStore((state) => state);
@@ -14,10 +25,24 @@ const SignOutButton = () => {
     clearUser();
     router.push('/');
   };
+
   return (
-    <button className="block w-full text-left px-2 py-2 text-sm letter-spacing color-dropdown" onClick={signOut}>
-      로그아웃
-    </button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <button className="block w-full text-left px-2 py-2 text-sm letter-spacing color-dropdown" onClick={signOut}>
+          로그아웃
+        </button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>로그아웃이 완료되었습니다.</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          {/* <AlertDialogCancel>아니요</AlertDialogCancel> */}
+          <AlertDialogAction onClick={() => signOut()}>확인</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
