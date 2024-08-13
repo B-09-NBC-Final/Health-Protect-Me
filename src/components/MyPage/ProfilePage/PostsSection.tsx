bf0bb7028b7e0792cf0cd629b5e777a54a832d98
Joohwan Seo <songqkaoswe@naver.com>
2024-08-13 23:17:48 +0900

    add: 마이페이지 내 스크롤 구현

--- src/components/MyPage/ProfilePage/PostsSection.tsx
+++ src/components/MyPage/ProfilePage/PostsSection.tsx
@@ -35,7 +35,11 @@ const PostsSection = () => {
 
       const userId = sessionData.session.user.id;
 
-      const { data, error } = await supabase.from('posts').select('*').eq('user_id', userId);
+      const { data, error } = await supabase
+        .from('posts')
+        .select('*')
+        .eq('user_id', userId)
+        .order('created_at', { ascending: false });
 
       if (error) {
         throw new Error(error.message);
@@ -54,32 +58,34 @@ const PostsSection = () => {
   return (
     <section className="w-[880px] px-10 py-6 border border-[#D5D6D8] rounded-2xl bg-white">
       <h2 className="text-2xl font-medium mb-4 text-gray900">내가 작성한 글</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-        {posts.length > 0 ? (
-          posts.map((post) => (
-            <article key={post.id}>
-              <Link href={`/posting-detail/${post?.id}`}>
-                <div className="w-60 h-48 mb-4 relative">
-                  <Image
-                    className="object-cover rounded-lg"
-                    src={post.image_url[0] || myPageDefaultImg}
-                    alt={`Post ${post.id}`}
-                    layout="fill"
-                  />
-                </div>
-                <h3 className="text-primary600 text-sm font-semibold mb-2 truncate">{post.category}</h3>
-                <p className="text-gray900 font-semibold mb-1 truncate">{post.title}</p>
-                <p className="text-gray800 text-sm mb-2 line-clamp-2 h-10">{post.content}</p>
-                <time className="text-gray600 text-sm">{new Date(post.created_at).toLocaleDateString('ko-KR')}</time>
-              </Link>
-            </article>
-          ))
-        ) : (
-          <div>
-            <p className="text-[#404145]">아직 작성된 글이 존재하지 않아요.</p>
-            <p className="text-[#404145]">첫번째 글을 작성하고 커뮤니티를 시작해보세요!</p>
-          </div>
-        )}
+      <div className="h-[600px] overflow-y-auto pr-4 custom-scrollbar">
+        <div className="grid grid-cols-3 gap-6">
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <article key={post.id} className="mb-6">
+                <Link href={`/posting-detail/${post?.id}`}>
+                  <div className="w-full h-48 mb-4 relative">
+                    <Image
+                      className="object-cover rounded-lg"
+                      src={post.image_url[0] || myPageDefaultImg}
+                      alt={`Post ${post.id}`}
+                      layout="fill"
+                    />
+                  </div>
+                  <h3 className="text-primary600 text-sm font-semibold mb-2 truncate">{post.category}</h3>
+                  <p className="text-gray900 font-semibold mb-1 truncate">{post.title}</p>
+                  <p className="text-gray800 text-sm mb-2 line-clamp-2 h-10">{post.content}</p>
+                  <time className="text-gray600 text-sm">{new Date(post.created_at).toLocaleDateString('ko-KR')}</time>
+                </Link>
+              </article>
+            ))
+          ) : (
+            <div className="col-span-3">
+              <p className="text-[#404145]">아직 작성된 글이 존재하지 않아요.</p>
+              <p className="text-[#404145]">첫번째 글을 작성하고 커뮤니티를 시작해보세요!</p>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
