62547d206c9a14ddf4a459c2c36453b9b47ede28
heesu21 <gmltncpfl1@naver.com>
2024-08-13 23:40:19 +0900

    feat: 로그아웃 여부 알럿

--- src/components/Common/Header/ProfileTrigger.tsx
+++ src/components/Common/Header/ProfileTrigger.tsx
@@ -1,13 +1,16 @@
-
 import React from 'react';
 import ProfileImg from '@/components/Common/ProfileImg';
 import Defaultimg from '@/assets/image/defaultimg.png';
 import { useUserStore } from '@/store/userStore';
 
-const ProfileTrigger = ({ onClick }: { onClick: () => void }) => {
+const ProfileTrigger = ({ setIsOpen }: { setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
   const { user } = useUserStore((state) => state);
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    setIsOpen((prev) => !prev);
+  };
   return (
-    <div onClick={onClick} className="border rounded-full overflow-hidden cursor-pointer">
+    <div onClick={handleClick} className="border rounded-full overflow-hidden cursor-pointer">
       <ProfileImg
         src={user?.profile_url && user.profile_url.length > 0 ? user.profile_url : Defaultimg}
         width={40}
