3c5b8cd54814f1171e5977a6c15e0a9d34f26f25
Elephant24456 <rlaxortn0984@naver.com>
2024-08-15 19:49:38 +0900

    fix: card padding

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col p-6", className)}
+    className={cn("flex flex-col px-6 py-4", className)}
     {...props}
   />
 ))
@@ -60,7 +60,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("px-6 pb-4", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
