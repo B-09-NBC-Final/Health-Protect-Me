4c5e42d8c6c1fa9cc422998c9d0eada13f4bca22
heesu21 <gmltncpfl1@naver.com>
2024-08-16 17:51:39 +0900

    fix: 폰트최적화 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,15 @@ const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="preload"
+          href="/fonts/subset-PretendardVariable-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>
         <Provider>
           <UserProvider>{children}</UserProvider>
