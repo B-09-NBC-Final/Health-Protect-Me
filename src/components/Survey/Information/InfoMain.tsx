6b686fcc0f8b4163289f7f2030eb2a3af861b78b
Joohwan Seo <songqkaoswe@naver.com>
2024-08-18 22:27:50 +0900

    fix: 설문조사 메인 디자인 수정

--- src/components/Survey/Information/InfoMain.tsx
+++ src/components/Survey/Information/InfoMain.tsx
@@ -11,8 +11,8 @@ const InfoMain = () => {
         나의 건강한 일상을 위해 설문을 입력하고<br className="hidden s:inline " /> 나만의 AI 헬스케어 전문가들이 매일 새로운 식단과 운동을 추천해드립니다.
       </p>
       <div className="flex justify-center mt-20 s:mt-20 s:mb-[388px]">
-        <Button className=" shadow-lg shadow-[#FF857A] w-56 h-12 s:w-[320px] p-[16px]  bg-[#FF7A85] text-white text-base rounded-lg hover:bg-[#FF7A85] transition duration-300">
-          <Link href="/info-reaserch">진단하러 가기</Link>
+        <Button className=" shadow-[#FF857A] w-56 h-12 s:w-[320px] p-[16px]  bg-[#FF7A85] text-white text-base rounded-lg hover:shadow-main-btn-hover hover:bg-pramary600  transition duration-300">
+          <Link href="/info-reaserch" className='font-semibold'>진단하러 가기</Link>
         </Button>
       </div>
     </div>
