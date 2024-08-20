4052adac1aeb4eaece2044c44fdf4ed13c981b0e
inhwa <jeoninhwa3@naver.com>
2024-08-20 18:20:34 +0900

    feat: 유저정보 없을 시 댓글창 안보이게 구현

--- src/components/Contents/PostingDetailPage/PostingComments.tsx
+++ src/components/Contents/PostingDetailPage/PostingComments.tsx
@@ -3,6 +3,7 @@
 import { useUserStore } from '@/store/userStore';
 import { createClient } from '@/supabase/client';
 import { Comments, Post } from '@/types';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 type newComments = Comments & {
@@ -18,6 +19,7 @@ const PostingComments = ({ post }: { post: Post }) => {
   const [isEditing, setIsEditing] = useState<number | null>(null);
   const { user } = useUserStore((state) => state);
   const supabase = createClient();
+  const router = useRouter();
   const dayjs = require('dayjs');
   const formatDate = (date: string) => dayjs(date).format('YY.MM.DD');
 
@@ -47,6 +49,7 @@ const PostingComments = ({ post }: { post: Post }) => {
 
   const submitComment = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     const { data: commentData, error } = await supabase.from('comments').insert({
       user_id: user?.userId as string,
       content: comment,
@@ -82,25 +85,29 @@ const PostingComments = ({ post }: { post: Post }) => {
 
   return (
     <div className="mt-10 pb-16">
-      <p className="text-sm text-gray900 font-semibold pb-4">
+      <p className="text-sm text-gray900 font-semibold pb-4 border-b border-solid border-gray200">
         댓글<span className="ml-1">{commentList?.length}</span>
       </p>
-      <form onSubmit={submitComment} className="flex border-t border-solid border-gray200 pt-6">
-        <input
-          className="w-full border border-solid border-gray300 py-2 px-3 rounded-lg text-sm"
-          type="text"
-          placeholder="댓글을 작성해 보세요."
-          maxLength={300}
-          onChange={(e) => setComment(e.target.value)}
-          value={comment}
-        />
-        <button
-          className="w-20 text-sm text-gray900 py-2 border border-solid border-gray400 bg-white rounded ml-2"
-          type="submit"
-        >
-          등록
-        </button>
-      </form>
+      {!user || user.is_survey_done ? (
+        ''
+      ) : (
+        <form onSubmit={submitComment} className="flex pt-6">
+          <input
+            className="w-full border border-solid border-gray300 py-2 px-3 rounded-lg text-sm"
+            type="text"
+            placeholder="댓글을 작성해 보세요."
+            maxLength={300}
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <button
+            className="w-20 text-sm text-gray900 py-2 border border-solid border-gray400 bg-white rounded ml-2"
+            type="submit"
+          >
+            등록
+          </button>
+        </form>
+      )}
 
       <ul>
         {commentList?.map((comment, idx) => (
@@ -115,14 +122,14 @@ const PostingComments = ({ post }: { post: Post }) => {
                         <button
                           type="button"
                           onClick={() => updateComment(comment.id, newContent)}
-                          className="text-sm text-primary600 border border-solid border-primary500 rounded px-2 py-[2px] ml-1 hover:bg-pramary100"
+                          className="text-sm text-primary600 border border-solid border-primary500 bg-white rounded px-2 py-[2px] ml-1 hover:bg-pramary100"
                         >
                           저장
                         </button>
                         <button
                           type="button"
                           onClick={() => setIsEditing(null)}
-                          className="text-sm text-gray900 border border-solid border-gray-400 rounded px-2 py-[2px] ml-1 hover:border-gray-400  hover:bg-gray-100"
+                          className="text-sm text-gray900 border border-solid border-gray-400 bg-white rounded px-2 py-[2px] ml-1 hover:border-gray-400  hover:bg-gray-100"
                         >
                           취소
                         </button>
@@ -135,14 +142,14 @@ const PostingComments = ({ post }: { post: Post }) => {
                             setIsEditing(comment.id);
                             setNewContent(comment.content);
                           }}
-                          className="text-sm border border-solid text-primary600 border-primary500 rounded px-2 py-[2px] hover:bg-pramary100"
+                          className="text-sm border border-solid text-primary600 border-primary500 bg-white rounded px-2 py-[2px] hover:bg-pramary100"
                         >
                           수정
                         </button>
                         <button
                           type="button"
                           onClick={() => deleteComment(comment.id)}
-                          className="text-sm border border-solid text-gray900  border-gray-400 rounded px-2 py-[2px] ml-1 hover:border-gray-400 hover:bg-gray-100"
+                          className="text-sm border border-solid text-gray900  border-gray-400 rounded bg-white px-2 py-[2px] ml-1 hover:border-gray-400 hover:bg-gray-100"
                         >
                           삭제
                         </button>
@@ -157,16 +164,16 @@ const PostingComments = ({ post }: { post: Post }) => {
                   type="text"
                   value={newContent}
                   onChange={(e) => setNewContent(e.target.value)}
-                  className="inline-block mt-2 bg-[#FFF1F0] rounded-lg py-2 px-3 text-gray800 text-sm w-full"
+                  className="inline-block mt-1 bg-white border border-solid border-gray300 rounded-lg py-2 px-3 text-gray800 text-sm w-full"
                   maxLength={300}
                 />
               ) : (
-                <p className="inline-block mt-2 bg-[#FFF1F0] rounded-lg py-2 px-3 text-gray800 text-sm">
+                <p className="inline-block mt-1 bg-[#FFF1F0] rounded-lg py-2 px-3 text-gray800 text-sm">
                   {comment.content}
                 </p>
               )}
             </div>
-            <p className="mt-2 text-gray600 text-sm">{formatDate(comment.created_at)}</p>
+            <p className="mt-2 text-xs text-gray600">{formatDate(comment.created_at)}</p>
           </li>
         ))}
       </ul>
