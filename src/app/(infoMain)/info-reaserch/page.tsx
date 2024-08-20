70836f1801e2edb279420ff0749f334065416e24
Elephant24456 <rlaxortn0984@naver.com>
2024-08-20 19:32:13 +0900

    fix: address

--- src/app/(infoMain)/info-reaserch/page.tsx
+++ src/app/(infoMain)/info-reaserch/page.tsx
@@ -1,11 +1,11 @@
-import InfoResearch from '@/components/Survey/Information/Reaserch/InfoResearch';
+import InfoResearch from '@/components/SurveyPage/Info-main/InfoResearch';
 import React from 'react';
 
 const InfoReaserchPage = () => {
   return (
     <div>
       <InfoResearch />
-      
+
     </div>
   );
 };
