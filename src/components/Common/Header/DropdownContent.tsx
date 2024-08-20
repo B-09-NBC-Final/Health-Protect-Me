99750f1f7807e87e469406910cb28772cb905c83
heesu21 <gmltncpfl1@naver.com>
2024-08-20 09:38:00 +0900

    feat: 드롭다운 css 수정

--- src/components/Common/Header/DropdownContent.tsx
+++ src/components/Common/Header/DropdownContent.tsx
@@ -8,7 +8,7 @@ type DropdownContentProps = {
 
 const DropdownContent: React.FC<DropdownContentProps> = ({ closeDropdown }) => {
   return (
-    <div className="absolute -left-[86px] px-[10px] py-2 mt-2 w-[102px] bg-white border border-gray-300 rounded-lg shadow-lg ">
+    <div className="absolute -left-[62px] px-[10px] py-2 mt-2 w-[102px] bg-white border border-gray-300 rounded-lg shadow-lg ">
       <Link
         href={'/info-detail'}
         className="block md:hidden px-2 py-2 text-sm letter-spacing color-dropdown"
@@ -18,12 +18,12 @@ const DropdownContent: React.FC<DropdownContentProps> = ({ closeDropdown }) => {
       </Link>
       <Link
         href={'/posting-main'}
-        className="block md:hidden px-2 py-2 text-sm letter-spacing color-dropdown"
+        className="block md:hidden px-2 py-1 text-sm letter-spacing color-dropdown"
         onClick={closeDropdown}
       >
         커뮤니티
       </Link>
-      <Link href={'/my-page'} className="block px-2 py-2 text-sm letter-spacing color-dropdown" onClick={closeDropdown}>
+      <Link href={'/my-page'} className="block px-2 py-1 text-sm letter-spacing color-dropdown" onClick={closeDropdown}>
         나의 프로필
       </Link>
       <SocialLogout />
